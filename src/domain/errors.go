c661d293bf71dc0880d9d109f95367a60f415831
cafefarm-johnny <npcdja@gmail.com>
2021-07-08 13:55:11 +0900

    feat: 도메인 글로벌 에러 구조체 정의

---
+++ src/domain/errors.go
@@ -0,0 +1,32 @@
+package domain
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	ErrDuplicatedUser = echo.NewHTTPError(
+		http.StatusUnauthorized,
+		NewDomainErr(401001, "이미 존재하는 회원입니다."),
+	)
+)
+
+type domainErr struct {
+	Code    int
+	Message string
+}
+
+// error 인터페이스 덕타이핑
+func (d *domainErr) Error() string {
+	return fmt.Sprintf("{ code: %d, message: %s }", d.Code, d.Message)
+}
+
+func NewDomainErr(code int, message string) *domainErr {
+	return &domainErr{
+		code,
+		message,
+	}
+}
