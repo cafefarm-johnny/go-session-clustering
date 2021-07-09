40c990701b18d4c6106f65c8c8a28c1d46a8a95e
cafefarm-johnny <npcdja@gmail.com>
2021-07-09 16:49:19 +0900

    feat: HTTP 로그 설정, 타임아웃 설정 추가

--- src/www/server.go
+++ src/www/server.go
@@ -3,6 +3,7 @@ package www
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gorilla/sessions"
 
@@ -18,12 +19,13 @@ const (
 
 func StartUp(e *echo.Echo) {
 	e.Use(
-		middleware.Logger(),
 		middleware.CSRF(),
 		middleware.Gzip(),
+		middleware.LoggerWithConfig(loggerConfig()),
 		middleware.RequestID(),
 		middleware.Recover(),
-		session.Middleware(sessions.NewCookieStore([]byte("secret"))),
+		middleware.TimeoutWithConfig(timeoutConfig()),
+		session.Middleware(sessionCookieStore()),
 	)
 
 	e.Validator = newArgumentValidator()
@@ -31,5 +33,30 @@ func StartUp(e *echo.Echo) {
 	if err := e.Start(port); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func loggerConfig() middleware.LoggerConfig {
+	return middleware.LoggerConfig{
+		Format: "{\n    " +
+			"timestamp: \"${time_rfc3339}\" \n    " +
+			"request-id: \"${id}\" \n    " +
+			"remote-ip: \"${remote_ip}\" \n    " +
+			"user-agent: \"${user_agent}\" \n    " +
+			"method: \"${method}\" \n    " +
+			"path: \"${path}\" \n    " +
+			"status: ${status} \n    " +
+			"error: \"${error}\" \n" +
+			"}",
+	}
+}
+
+func sessionCookieStore() *sessions.CookieStore {
+	return sessions.NewCookieStore([]byte("secret"))
+}
+
+func timeoutConfig() middleware.TimeoutConfig {
+	return middleware.TimeoutConfig{
+		Timeout: 30 * time.Second,
 	}
 }
